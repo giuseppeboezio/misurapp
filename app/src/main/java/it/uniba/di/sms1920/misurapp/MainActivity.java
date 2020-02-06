f98161c597fe193e72225c6af4718591db0a2de0
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2020-02-06 09:05:54 +0100

    First commit

---
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/MainActivity.java
@@ -0,0 +1,14 @@
+package it.uniba.di.sms1920.misurapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
