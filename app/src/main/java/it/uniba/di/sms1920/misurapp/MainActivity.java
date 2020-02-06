bb86be3779fca66a15f1233d69a0fb4ee0914bd7
Vito <costanzovito@gmail.com>
2020-02-06 13:06:48 +0100

    MainActivity partial developed

--- app/src/main/java/it/uniba/di/sms1920/misurapp/MainActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/MainActivity.java
@@ -2,6 +2,9 @@ package it.uniba.di.sms1920.misurapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setLogo(R.mipmap.ic_launcher_round);
+        myToolbar.setTitle(R.string.app_name);
+        myToolbar.setTitleTextColor(getResources().getColor(R.color.colorOnPrimary));
+        setSupportActionBar(myToolbar);
     }
 }
