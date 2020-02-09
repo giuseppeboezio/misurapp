461e4d28cdfb956f67f8f9aac62f5000201705dc
AKiSADev <vivianalamboni@gmail.com>
2020-02-09 19:33:34 +0100

    Proximity partially developed

--- app/src/main/java/it/uniba/di/sms1920/misurapp/MotionActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/MotionActivity.java
@@ -2,13 +2,41 @@ package it.uniba.di.sms1920.misurapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MotionActivity extends AppCompatActivity {
+
+
+    private Button buttonProximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motion);
+
+        buttonProximity =  (Button) findViewById(R.id.btnSensorProximity);
+
+
+        buttonProximity.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                goToProximitySensorActivity();
+
+            }
+
+        });
+
+
     }
+
+    private void goToProximitySensorActivity(){
+        Intent intent = new Intent(this, SensorProximityActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/it/uniba/di/sms1920/misurapp/SensorProximityActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/SensorProximityActivity.java
@@ -7,21 +7,38 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SensorProximityActivity extends AppCompatActivity implements SensorEventListener {
 
+    private static final float PIVOT_X = (float) 0.5;
+    private static final float PIVOT_Y = (float) 0.5;
+
     private SensorManager mSensorManager;
     private Sensor mSensorProximity;
+    private ImageView highFiveHand;
+    private ScaleAnimation scaleAnimation;
+    private float fromX, fromY, toX, toY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_proximity);
 
+        highFiveHand = (ImageView) findViewById(R.id.img_hand);
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        toX = 1;
+        toY = 1;
+
     }
 
     @Override
@@ -32,6 +49,34 @@ public class SensorProximityActivity extends AppCompatActivity implements Sensor
     @Override
     public final void onSensorChanged(SensorEvent event) {
         float distance = event.values[0];
+
+        fromX = toX;
+        fromY = toY;
+
+        if (distance >= 10){
+
+            toX = 1;
+            toY = 1;
+        }
+        else if (distance > 4 && distance < 10){
+
+            toX = (float) 1.3;
+            toY = (float) 1.3;
+
+        }
+        else {
+
+            toX = (float) 1.7;
+            toY = (float) 1.7;
+
+        }
+
+        scaleAnimation = new ScaleAnimation(fromX, toX, fromY, toY);
+        scaleAnimation.setDuration(200);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setInterpolator(new LinearInterpolator());
+
+        highFiveHand.setAnimation(scaleAnimation);
 
     }
 
