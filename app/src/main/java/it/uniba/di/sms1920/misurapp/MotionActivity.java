9eff3591618478ec7023465671dcb681000721e1
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2020-02-10 11:50:51 +0100

    MotionActivity fully developed

--- app/src/main/java/it/uniba/di/sms1920/misurapp/MotionActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/MotionActivity.java
@@ -1,41 +1,85 @@
 package it.uniba.di.sms1920.misurapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MotionActivity extends AppCompatActivity {
 
 
-    private Button buttonProximity;
+    private SensorManager mSensorManager;
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorStepCounter;
+    private CardView mCardAccelerometer;
+    private CardView mCardStepCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motion);
 
-        buttonProximity =  (Button) findViewById(R.id.btnSensorProximity);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorStepCounter = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        buttonProximity.setOnClickListener(new Button.OnClickListener(){
+        //Setting Accelerometer Card
 
-            @Override
-            public void onClick(View v){
+        mCardAccelerometer = (CardView) findViewById(R.id.cardAccelerometer);
 
-                goToProximitySensorActivity();
+        if(mSensorAccelerometer != null) {
+            mCardAccelerometer.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    goSensorAccelerometer();
+                }
+            });
+        } else {
+            mCardAccelerometer.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MotionActivity.this, getResources().getString(R.string.withoutAccelerometerSensor), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 
-            }
+        //Setting Step Counter Card
 
-        });
+        mCardStepCounter = (CardView) findViewById(R.id.cardStepCounter);
 
+        if(mCardStepCounter != null) {
+            mCardStepCounter.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    goSensorStepCounter();
+                }
+            });
+        } else {
+            mCardStepCounter.setOnClickListener(new Button.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MotionActivity.this, getResources().getString(R.string.withoutStepCounterSensor), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 
     }
 
-    private void goToProximitySensorActivity(){
-        Intent intent = new Intent(this, SensorProximityActivity.class);
+    private void goSensorAccelerometer() {
+        Intent intent = new Intent(this, SensorAccelerometerActivity.class);
+        startActivity(intent);
+    }
+
+    private void goSensorStepCounter(){
+        Intent intent = new Intent(this, SensorHumidityActivity.class);
         startActivity(intent);
     }
 
