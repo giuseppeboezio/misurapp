d7b159187fbf96735230224e61b8ec3adadd4dd5
Vincenzo Conga <vincenzoconga@gmail.com>
2020-02-10 12:57:57 +0100

    light sensor

--- app/src/main/java/it/uniba/di/sms1920/misurapp/SensorLightActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/SensorLightActivity.java
@@ -8,6 +8,9 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SensorLightActivity extends AppCompatActivity implements SensorEventListener {
@@ -15,6 +18,9 @@ public class SensorLightActivity extends AppCompatActivity implements SensorEven
     private TextView mTxtValue;
     private SensorManager mSensorManager;
     private Sensor mSensorLight;
+    private ImageView mSunglasses;
+    private AlphaAnimation mAlphaAnimation;
+    private float fromAlpha, toAlpha;
 
 
     @Override
@@ -25,7 +31,8 @@ public class SensorLightActivity extends AppCompatActivity implements SensorEven
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        mTxtValue = (TextView) findViewById(R.id.txtValue);
+        mSunglasses = (ImageView) findViewById(R.id.imageSunglasses);
+        toAlpha = (float) 0.3;
 
     }
 
@@ -37,7 +44,36 @@ public class SensorLightActivity extends AppCompatActivity implements SensorEven
     @Override
     public final void onSensorChanged(SensorEvent event) {
         float lux = event.values[0];
-        mTxtValue.setText(String.valueOf(lux)); //TODO animation to implement
+        fromAlpha = toAlpha;
+
+        if(lux >= 0 && lux < 4001){
+            toAlpha = (float) 0.1;
+        } else if(lux > 4000 && lux < 8001){
+            toAlpha = (float) 0.2;
+        } else if(lux > 8000 && lux < 12001) {
+            toAlpha = (float) 0.3;
+        } else if(lux > 12000 && lux < 16001) {
+            toAlpha = (float) 0.4;
+        } else if(lux > 16000 && lux < 20001) {
+            toAlpha = (float) 0.5;
+        } else if(lux > 20000 && lux < 24001) {
+            toAlpha = (float) 0.6;
+        } else if(lux > 24000 && lux < 28001) {
+            toAlpha = (float) 0.7;
+        } else if(lux > 28000 && lux < 32001) {
+            toAlpha = (float) 0.8;
+        } else if(lux > 32000 && lux < 36001) {
+            toAlpha = (float) 0.9;
+        } else if(lux > 36000 && lux < 40001) {
+            toAlpha = (float) 1.0;
+        }
+
+        mAlphaAnimation = new AlphaAnimation(fromAlpha, toAlpha);
+        mAlphaAnimation.setDuration(200);
+        mAlphaAnimation.setFillAfter(true);
+        mAlphaAnimation.setInterpolator(new LinearInterpolator());
+        mSunglasses.setAnimation(mAlphaAnimation);
+
     }
 
     @Override
