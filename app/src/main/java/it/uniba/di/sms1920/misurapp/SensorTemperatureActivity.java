7b868eebaa8444ea143a258d7a0983285c1bd28e
Vincenzo Conga <vincenzoconga@gmail.com>
2020-02-14 18:17:38 +0100

    graphic fix 2

--- app/src/main/java/it/uniba/di/sms1920/misurapp/SensorTemperatureActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/SensorTemperatureActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,8 +12,10 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,12 +30,21 @@ public class SensorTemperatureActivity extends AppCompatActivity implements Sens
     private Set<Detection> detections;
     private SensorManager mSensorManager;
     private Sensor mSensorTemperature;
+    private Button mBtnHistory;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_temperature);
+
+        mBtnHistory = (Button) findViewById(R.id.btnHistory);
+        mBtnHistory.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                goHistory();
+            }
+        });
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
@@ -123,6 +135,12 @@ public class SensorTemperatureActivity extends AppCompatActivity implements Sens
         String message = getResources().getQuantityString(R.plurals.saveMessage, quantity, quantity);
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         detections.clear();
+    }
+
+    private void goHistory() {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        intent.putExtra(DetectionOpenHelper.SENSOR_TYPE, Sensor.TYPE_AMBIENT_TEMPERATURE);
+        startActivity(intent);
     }
 
     @Override
