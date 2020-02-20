e2fe882995d9828b699696d8d9b7505ccfd6f62f
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2020-02-20 19:40:21 +0100

    Sensor temperature finally developed

--- app/src/main/java/it/uniba/di/sms1920/misurapp/SensorTemperatureActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/misurapp/SensorTemperatureActivity.java
@@ -23,6 +23,11 @@ import java.util.Set;
 
 public class SensorTemperatureActivity extends AppCompatActivity implements SensorEventListener {
 
+    private final static float MIN_TEMPERATURE = 186.8f;
+    private final static int DIVISION_FACTOR = 100;
+    private final static float MULTIPLICATION_FACTOR = 2.41f;
+    private int [] temperatureImages;
+    private int mIndex;
     private ImageView temperature;
     private TextView showTemperatureData;
     private Set<Detection> detections;
@@ -50,6 +55,10 @@ public class SensorTemperatureActivity extends AppCompatActivity implements Sens
         myToolbar.setTitleTextColor(getResources().getColor(R.color.colorOnPrimary));
         setSupportActionBar(myToolbar);
 
+        temperatureImages = new int[] {R.drawable.cold5, R.drawable.cold4, R.drawable.cold3,
+                R.drawable.cold2, R.drawable.cold1, R.drawable.red1, R.drawable.red2, R.drawable.red3,
+                R.drawable.red4, R.drawable.red5};
+
         temperature = findViewById(R.id.img_temp);
         showTemperatureData = findViewById(R.id.temperatureData);
         detections = new HashSet<Detection>();
@@ -70,31 +79,14 @@ public class SensorTemperatureActivity extends AppCompatActivity implements Sens
     public final void onSensorChanged(SensorEvent event) {
        float ambientTemperature = event.values[0];
 
+        mIndex = Math.round((ambientTemperature + MIN_TEMPERATURE) / DIVISION_FACTOR * MULTIPLICATION_FACTOR);
+        if(mIndex < 0) {
+            mIndex = 0;
+        }
 
-       if(ambientTemperature < -30) {
-           temperature.setImageResource(R.drawable.cold5);
-       } else if (ambientTemperature >= -30 && ambientTemperature < -20) {
-           temperature.setImageResource(R.drawable.cold4);
-       } else if (ambientTemperature >= -20 && ambientTemperature < -10) {
-           temperature.setImageResource(R.drawable.cold3);
-       } else if (ambientTemperature >= -10 && ambientTemperature < 0) {
-           temperature.setImageResource(R.drawable.cold2);
-       } else if (ambientTemperature >= 0 && ambientTemperature < 10) {
-           temperature.setImageResource(R.drawable.cold1);
-       } else if (ambientTemperature >= 10 && ambientTemperature < 20) {
-           temperature.setImageResource(R.drawable.red1);
-       } else if (ambientTemperature >= 20 && ambientTemperature < 30) {
-           temperature.setImageResource(R.drawable.red2);
-       } else if (ambientTemperature >= 30 && ambientTemperature < 40) {
-           temperature.setImageResource(R.drawable.red3);
-       } else if (ambientTemperature >= 40 && ambientTemperature < 50) {
-           temperature.setImageResource(R.drawable.red4);
-       } else {
-           temperature.setImageResource(R.drawable.red5);
-       }
+        temperature.setImageResource(temperatureImages[mIndex]);
 
-
-        showTemperatureData.setText(String.valueOf(ambientTemperature) + "  °C");
+        showTemperatureData.setText(String.valueOf(ambientTemperature) + getString(R.string.unit_temperature));
 
         Detection temperatureDetection = new Detection();
 
